3cb12beee27993ccdb9a632219c55932bb18d6ca
Khan Moin <134956998+CodeByMoin@users.noreply.github.com>
2024-08-16 14:17:15 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,166 +1,154 @@
-/*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-navToggle = document.getElementById('nav-toggle'),
-navClose = document.getElementById('nav-close');
+let emailjsServiceID, emailjsTemplateID, emailjsPublicKey;
 
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
-if(navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show');
-    });
-}
+        // Fetch API keys from the serverless function
+        fetch('/api/keys')
+            .then(response => response.json())
+            .then(data => {
+                emailjsServiceID = data.emailjs_serviceID;
+                emailjsTemplateID = data.emailjs_templateID;
+                emailjsPublicKey = data.emailjs_publickey;
 
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-if(navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show');
-    });
-}
+                // Initialize EmailJS
+                emailjs.init(emailjsPublicKey);
 
-/*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link');
+                // Your existing JavaScript code
 
-function linkAction() {
-    const navMenu = document.getElementById('nav-menu');
-    // when we click on each nav__link, we remove the show class
-    navMenu.classList.remove('show');
-} 
+                const navMenu = document.getElementById('nav-menu'),
+                navToggle = document.getElementById('nav-toggle'),
+                navClose = document.getElementById('nav-close');
 
-navLink.forEach((n) => n.addEventListener('click', linkAction));
+                /*===== MENU SHOW =====*/
+                if(navToggle) {
+                    navToggle.addEventListener('click', () => {
+                        navMenu.classList.add('show');
+                    });
+                }
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]');
+                /*===== MENU HIDDEN =====*/
+                if(navClose) {
+                    navClose.addEventListener('click', () => {
+                        navMenu.classList.remove('show');
+                    });
+                }
 
-function scrollActive() {
-    const scrollY = window.pageYOffset;
+                /*==================== REMOVE MENU MOBILE ====================*/
+                const navLink = document.querySelectorAll('.nav__link');
 
-    sections.forEach((current) => {
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 50;
-        const sectionId = current.getAttribute('id');
+                function linkAction() {
+                    const navMenu = document.getElementById('nav-menu');
+                    navMenu.classList.remove('show');
+                } 
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
-        } else {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
-        }
-    });
-}
+                navLink.forEach((n) => n.addEventListener('click', linkAction));
 
-window.addEventListener('scroll', scrollActive);
+                /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+                const sections = document.querySelectorAll('section[id]');
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/
-function scrollHeader() {
-    const header = document.getElementById('header');
-    // when the scroll is greater than 80 viewport height, add the scroll-header class to header tag
-    if (this.scrollY >= 80) header.classList.add('scroll-header');
-    else header.classList.remove('scroll-header');
-}
+                function scrollActive() {
+                    const scrollY = window.pageYOffset;
 
-window.addEventListener('scroll', scrollHeader);
+                    sections.forEach((current) => {
+                        const sectionHeight = current.offsetHeight;
+                        const sectionTop = current.offsetTop - 50;
+                        const sectionId = current.getAttribute('id');
 
-/*==================== SHOW SCROLL UP ====================*/
-function scrollUp() {
-    const scrollUp = document.getElementById('scroll-up');
-    // when the scroll is greater than 350 viewport height, add the show-scroll class to scroll-up tag
-    if (this.scrollY >= 350) scrollUp.classList.add('show-scroll');
-    else scrollUp.classList.remove('show-scroll');
-}
+                        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+                        } else {
+                            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+                        }
+                    });
+                }
 
-window.addEventListener('scroll', scrollUp);
+                window.addEventListener('scroll', scrollActive);
 
-/*==================== ABOUT TABS ====================*/
-const tabs = document.querySelectorAll('[data-target]'),
-tabsContents = document.querySelectorAll('[data-content]');
+                /*==================== CHANGE BACKGROUND HEADER ====================*/
+                function scrollHeader() {
+                    const header = document.getElementById('header');
+                    if (this.scrollY >= 80) header.classList.add('scroll-header');
+                    else header.classList.remove('scroll-header');
+                }
 
-tabs.forEach((tab) => {
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.target);
-        
-        tabsContents.forEach((tabContent) => {
-            tabContent.classList.remove('tab__active');
-        });
+                window.addEventListener('scroll', scrollHeader);
 
-        target.classList.add('tab__active');
+                /*==================== SHOW SCROLL UP ====================*/
+                function scrollUp() {
+                    const scrollUp = document.getElementById('scroll-up');
+                    if (this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+                    else scrollUp.classList.remove('show-scroll');
+                }
 
-        tabs.forEach((tab) => {
-            tab.classList.remove('tab__active');
-        });
+                window.addEventListener('scroll', scrollUp);
 
-        tab.classList.add('tab__active');
-    });
-});
+                /*==================== ABOUT TABS ====================*/
+                const tabs = document.querySelectorAll('[data-target]'),
+                tabsContents = document.querySelectorAll('[data-content]');
 
-/*=============== HEADER TABS =============== */
-const headerTabs = document.querySelectorAll('.nav__link');
+                tabs.forEach((tab) => {
+                    tab.addEventListener('click', () => {
+                        const target = document.querySelector(tab.dataset.target);
+                        
+                        tabsContents.forEach((tabContent) => {
+                            tabContent.classList.remove('tab__active');
+                        });
 
-headerTabs.forEach((tab) => {
-    tab.addEventListener('click', () => {
-        headerTabs.forEach((tab) => {
-            tab.classList.remove('active-link');
-        });
+                        target.classList.add('tab__active');
 
-        tab.classList.add('active-link');
-    });
-});
+                        tabs.forEach((tab) => {
+                            tab.classList.remove('tab__active');
+                        });
 
-/*=============== CONTACT FORM =============== */
-const contactForm = document.getElementById('contact-form'),
-contactName = document.getElementById('contact-name'),
-contactEmail = document.getElementById('contact-email'),
-contactSubject = document.getElementById('contact-subject'),
-contactMessage = document.getElementById('contact-message'),
-errorMessage = document.getElementById('error-message');
+                        tab.classList.add('tab__active');
+                    });
+                });
 
-const sendEmail = (e) => {
-    e.preventDefault();
+                /*=============== HEADER TABS =============== */
+                const headerTabs = document.querySelectorAll('.nav__link');
 
-    // Check if the field has a value
-    if (contactName.value === '' || contactEmail.value === '' || contactMessage.value === '' || contactSubject.value === '') {
-        // Show message
-        errorMessage.textContent = 'Write all the input fields';
-    } else {
-        // Prepare the data to send
-        const data = {
-            contactName: contactName.value,
-            contactEmail: contactEmail.value,
-            contactSubject: contactSubject.value,
-            contactMessage: contactMessage.value
-        };
+                headerTabs.forEach((tab) => {
+                    tab.addEventListener('click', () => {
+                        headerTabs.forEach((tab) => {
+                            tab.classList.remove('active-link');
+                        });
 
-        // Send data to the serverless function
-        fetch('assets/api/send-email', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-        .then(response => response.json())
-        .then(result => {
-            // Show success message
-            errorMessage.classList.add('color-first');
-            errorMessage.textContent = result.success || 'Message sent ✔️';
-            
-            // Remove message after 5 seconds
-            setTimeout(() => {
-                errorMessage.textContent = '';
-            }, 5000);
+                        tab.classList.add('active-link');
+                    });
+                });
 
-            // Clear input fields
-            contactName.value = '';
-            contactEmail.value = '';
-            contactSubject.value = '';
-            contactMessage.value = '';
-        })
-        .catch(error => {
-            // Handle error
-            errorMessage.textContent = 'Oops! Something went wrong...';
-            console.error('Error:', error);
-        });
-    }
-};
+                /*=============== CONTACT FORM =============== */
+                const contactForm = document.getElementById('contact-form'),
+                contactName = document.getElementById('contact-name'),
+                contactEmail = document.getElementById('contact-email'),
+                contactSubject = document.getElementById('contact-subject'),
+                contactMessage = document.getElementById('contact-message'),
+                errorMessage = document.getElementById('error-message');
 
-contactForm.addEventListener('submit', sendEmail);
+                const sendEmail = (e) => {
+                    e.preventDefault();
+
+                    if(contactName.value === '' || contactEmail.value === '' || contactMessage.value === '' || contactSubject.value === '') {
+                        errorMessage.textContent = 'Write all the input fields';
+                    } else {
+                        emailjs.sendForm(emailjsServiceID, emailjsTemplateID, '#contact-form', emailjsPublicKey)
+                            .then(() => {
+                                errorMessage.classList.add('color-first');
+                                errorMessage.textContent = 'Message sent ✔️';
+                                
+                                setTimeout(() => {
+                                    errorMessage.textContent = '';
+                                }, 5000);
+                            }, (error) => {
+                                errorMessage.textContent = 'Oops! Something went wrong...';
+                                console.error('Error:', error);
+                            });
+
+                        contactName.value = '';
+                        contactEmail.value = '';
+                        contactSubject.value = '';
+                        contactMessage.value = '';
+                    }
+                };
+
+                contactForm.addEventListener('submit', sendEmail);
+            })
+            .catch(error => console.error('Error fetching API keys:', error));
